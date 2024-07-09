dec8bd48e83bd9d40aefa130b3eddd2d811bc771
Pushpavalli Varthini M <132148416+varthini-mpv@users.noreply.github.com>
2024-07-10 00:36:43 +0530

    passwordgenerator.py

---
+++ passwordgenerator.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+def generate_password(length):
+   
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    
+    try:
+        length = int(input("Enter the desired length of the password: "))
+        if length <= 0:
+            print("Please enter a positive integer for the password length.")
+            return
+    except ValueError:
+        print("Please enter a valid integer for the password length.")
+        return
+    
+   
+    password = generate_password(length)
+    
+
+    print(f"Generated Password: {password}")
+
+if __name__ == "__main__":
+    main()
